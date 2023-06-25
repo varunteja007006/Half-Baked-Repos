14ada46d870d6d7559f1f9c13d8a860ecffb50c4
Varun <varunteja007006@gmail.com>
2023-06-25 15:00:13 +0530

    Learn children Props

--- Practise React/src/App.jsx
+++ Practise React/src/App.jsx
@@ -1,4 +1,5 @@
 import LearnCheckboxes from "./component/main/LearnCheckboxes";
+import LearnChildrenProp from "./component/main/LearnChildrenProp";
 import LearnFramer from "./component/main/LearnFramer";
 import LearnUseReducer from "./component/main/LearnUseReducer";
 import LearnUseRedux from "./component/main/LearnUseRedux";
@@ -6,6 +7,18 @@ import LearnUseRedux from "./component/main/LearnUseRedux";
 function App() {
   return (
     <div className="app p-5 flex flex-col gap-5 bg-emerald-100">
+      {/* I dont want to render the below paragraph for this component LearnChildrenProp */}
+      {/* <p>Hi there, How are you? 😃</p> */}
+      <LearnChildrenProp
+        title={"No Children Props"}
+        subtitle={"This is a demo"}
+      ></LearnChildrenProp>
+      <LearnChildrenProp
+        title={"There is a Children Props"}
+        subtitle={"This is a demo"}
+      >
+        <p>Hi there, How are you? 😃</p>
+      </LearnChildrenProp>
       <LearnFramer></LearnFramer>
       <LearnUseReducer></LearnUseReducer>
       <LearnUseRedux></LearnUseRedux>
--- Practise React/src/component/main/CodeBlock.jsx
+++ Practise React/src/component/main/CodeBlock.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function CodeBlock({ children }) {
-  return <div className="border-2 border-black p-5 bg-white">{children}</div>;
+  return <div className="border-2 border-black p-5 bg-yellow-100">{children}</div>;
 }
 
 export default CodeBlock;
