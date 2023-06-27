525c9c9037c52ceae962dff214e5118120749d55
Varun <varunteja007006@gmail.com>
2023-06-27 22:13:23 +0530

    Learn MongoDB

---
+++ mongodbtest/backend/models/sampleModel.js
@@ -0,0 +1,17 @@
+//Import Mongoose
+const mongoose = require("mongoose");
+//Get Schema from mongoose
+const Schema = mongoose.Schema;
+
+//Create the model using Schema
+const sampleSchema = new Schema(
+    {
+        name:{
+            type: String,
+            required: true,
+        }
+    },
+    { timestamps: true }
+)
+
+module.exports = mongoose.model("sample", sampleSchema);
