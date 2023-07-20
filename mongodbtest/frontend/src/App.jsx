a3a036f6e005b71273c9434516b98293e440c118
Varun <varunteja007006@gmail.com>
2023-07-20 23:31:18 +0530

    practise react query and mongodb

--- mongodbtest/frontend/src/App.jsx
+++ mongodbtest/frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import Homepage from "./pages/Homepage";
+
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="container">
+        <Homepage></Homepage>
       </div>
     </>
   );
--- mongodbtest/frontend/src/main.jsx
+++ mongodbtest/frontend/src/main.jsx
@@ -1,7 +1,12 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-)
+  </QueryClientProvider>
+);
