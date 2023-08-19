46837a7c906144114aa096935a43987480333405
Varun <varunteja007006@gmail.com>
2023-08-19 23:29:47 +0530

    Practise Python

--- Practise React/tailwind.config.js
+++ Practise React/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-  darkMode: 'class',
+  plugins: [daisyui],
+  darkMode: "class",
 };
