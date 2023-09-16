c789958869b1aa8b2a60f9251f32916b32b69279
Varun <varunteja007006@gmail.com>
2023-09-16 23:59:29 +0530

    practise GFG

--- Practise Python/python GFG/Search/TernarySearch.py
+++ Practise Python/python GFG/Search/TernarySearch.py
@@ -3,7 +3,7 @@
 import math as mt
 
 # Function to perform Ternary Search
-def ternarySearch(l, r, key, ar):
+def ternaryRecursiveSearch(l, r, key, ar):
 
 	if (r >= l):
 
@@ -25,17 +25,17 @@ def ternarySearch(l, r, key, ar):
 		if (key < ar[mid1]):
 
 			# The key lies in between l and mid1
-			return ternarySearch(l, mid1 - 1, key, ar)
+			return ternaryRecursiveSearch(l, mid1 - 1, key, ar)
 		
 		elif (key > ar[mid2]):
 
 			# The key lies in between mid2 and r
-			return ternarySearch(mid2 + 1, r, key, ar)
+			return ternaryRecursiveSearch(mid2 + 1, r, key, ar)
 		
 		else:
 
 			# The key lies in between mid1 and mid2
-			return ternarySearch(mid1 + 1,
+			return ternaryRecursiveSearch(mid1 + 1,
 								mid2 - 1, key, ar)
 		
 	# Key not found
@@ -59,8 +59,8 @@ r = 9
 # Key to be searched in the array
 key = 5
 
-# Search the key using ternarySearch
-p = ternarySearch(l, r, key, ar)
+# Search the key using ternaryRecursiveSearch
+p = ternaryRecursiveSearch(l, r, key, ar)
 
 # Print the result
 print("Index of", key, "is", p)
@@ -70,11 +70,76 @@ print("Index of", key, "is", p)
 # Key to be searched in the array
 key = 50
 
-# Search the key using ternarySearch
-p = ternarySearch(l, r, key, ar)
+# Search the key using ternaryRecursiveSearch
+p = ternaryRecursiveSearch(l, r, key, ar)
 
 # Print the result
 print("Index of", key, "is", p)
 
-# This code is contributed by
-# Mohit kumar 29
+
+# Python 3 program to illustrate iterative
+# approach to ternary search
+
+# Function to perform Ternary Search
+def ternaryIterativeSearch(l, r, key, ar):
+	while r >= l:
+		
+		# Find mid1 and mid2
+		mid1 = l + (r-l) // 3
+		mid2 = r - (r-l) // 3
+
+		# Check if key is at any mid
+		if key == ar[mid1]:
+			return mid1
+		if key == ar[mid2]:
+			return mid2
+
+		# Since key is not present at mid,
+		# Check in which region it is present
+		# Then repeat the search operation in that region
+		if key < ar[mid1]:
+			# key lies between l and mid1
+			r = mid1 - 1
+		elif key > ar[mid2]:
+			# key lies between mid2 and r
+			l = mid2 + 1
+		else:
+			# key lies between mid1 and mid2
+			l = mid1 + 1
+			r = mid2 - 1
+
+	# key not found
+	return -1
+
+# Driver code
+
+# Get the list
+# Sort the list if not sorted
+ar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Starting index
+l = 0
+
+# end element index
+r = 9
+
+# Checking for 5
+# Key to be searched in the list
+key = 5
+
+# Search the key using ternary search
+p = ternaryIterativeSearch(l, r, key, ar)
+
+# Print the result
+print("Index of", key, "is", p)
+
+# Checking for 50
+# Key to be searched in the list
+key = 50
+
+# Search the key using ternary search
+p = ternaryIterativeSearch(l, r, key, ar)
+
+# Print the result
+print("Index of", key, "is", p)
+
