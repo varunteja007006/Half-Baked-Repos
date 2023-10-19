2be7693623f9d220f90b344994cf860a7f87ed24
Varun <varunteja007006@gmail.com>
2023-10-19 12:37:23 +0530

    Updated the Repo

--- My Code Snippets/react+TS+Tailwind/README.md
+++
@@ -1,1 +0,0 @@
-https://youtu.be/bjnW2NLAofI
--- basicWebDev/sample.js
+++ basicWebDev/sample.js
@@ -1,8 +1,16 @@
 function alertMe() {
   alert("You clicked me !!!");
 }
-    const btn = document.getElementById("sampleClick");
-    btn.onclick = () => {
-      alert("this is weird!!");
-    };
-    console.log(btn);
+const btn = document.getElementById("sampleClick");
+btn.onclick = () => {
+  alert("this is weird!!");
+};
+
+const form = document.getElementById("handleForm");
+form.onsubmit = (e) => {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  console.log(data.get("name"));
+  const dataObj = Object.fromEntries(data.entries());
+  console.log(dataObj);
+};
