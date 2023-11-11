6a7ce337dc22e785daea65394be1443635456a2c
varun <varunteja007006@gmail.com>
2023-11-12 00:18:22 +0530

    Device update

--- Practice TS/sample.ts
+++ Practice TS/sample.ts
@@ -1,41 +1,5 @@
-class User {
-  email: string;
-  userID: number;
-
-  /*
-    private property cannot be accessed outside class but can be accessed inside class.
-
-    We can design a getter & setter methods so that if anyone wants to modify this private property 
-    they can do that indirectly
-
-    */
-  private courseCount: number = 1;
-
-  constructor(email: string, userID: number) {
-    this.email = email;
-    this.userID = userID;
-  }
-
-  get getMaskedEmail(): string {
-    return `${this.email.split("@")[0]}@test.com`;
-  }
-
-  get getCourseCount(): number {
-    return this.courseCount;
-  }
-
-  //   // setter cannot have return type annotation as well
-  //   set setCourseCount(newCourseCount): number {
-  //     return this.courseCount; // this throws an error because setter cannot have a return
-  //   }
-
-  set setCourseCount(newCourseCount: number) {
-    this.courseCount = newCourseCount; // this throws an error because setter cannot have a return
-  }
-}
-
-const user = new User("test@test.com", 1001);
+// Abstract classes
 
 export {};
 
-//3:16:00
+//3:35:37
