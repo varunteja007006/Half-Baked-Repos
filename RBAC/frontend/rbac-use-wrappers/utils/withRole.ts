35720f23359a65645f9a63ccc212b6917c1494df
Varun Teja <68656749+varunteja007006@users.noreply.github.com>
2024-11-22 20:44:43 +0530

    Update withRole.ts

--- RBAC/frontend/rbac-use-wrappers/utils/withRole.ts
+++ RBAC/frontend/rbac-use-wrappers/utils/withRole.ts
@@ -1,31 +1,27 @@
-"use client";
-import React from "react";
-
-import { Context, users, AppContext } from "@/utils/utils";
-
-const withRole =
-  (roles: Array<string>) =>
-  (Component: React.FC<any>, onDeny?: React.FC<any>) =>
-  (props: any) => {
-    // get current user roles from context (can be from token for from redux store)
-    const context: AppContext = React.useContext(Context);
-    if (!context) {
-      return null;
-    }
-
-    const user = context.state.user as keyof typeof users; // 'basicUser' | 'intermediate' | 'superUser'
-
-    const userRoles = users[user].roles || [];
-
-    const hasAccess =
-      userRoles.includes("superuser") ||
-      userRoles.some((r) => roles.includes(r));
-
-    if (hasAccess) return React.createElement(Component, props, null);
-
-    if (onDeny) return React.createElement(onDeny, props, null);
-
-    return null;
-  };
-
-export default withRole;
+ "use client";
+ import React from "react";
+ import { Context, users, AppContext } from "@/utils/utils";
+ 
+ const withRole = (roles: Array<string>) => 
+   (Component: React.ComponentType<any>, onDeny?: React.ComponentType<any>) => 
+     React.forwardRef((props: any, ref: any) => {
+       // get current user roles from context (can be from token or from redux store)
+       const context: AppContext = React.useContext(Context);
+       if (!context) {
+         return null;
+       }
+       
+       const user = context.state.user as keyof typeof users; // 'basicUser' | 'intermediate' | 'superUser'
+       const userRoles = users[user].roles || [];
+       
+       const hasAccess =
+         userRoles.includes("superuser") ||
+         userRoles.some((r) => roles.includes(r));
+       
+       if (hasAccess) return React.createElement(Component, { ...props, ref }, null);
+       if (onDeny) return React.createElement(onDeny, { ...props, ref }, null);
+       
+       return null;
+     });
+ 
+ export default withRole;
