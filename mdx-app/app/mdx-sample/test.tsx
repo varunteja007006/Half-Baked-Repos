41e14a8514c0eeb15397c4440fea3b0cc7438ffe
varunteja <varunteja007006@gmail.com>
2025-05-11 13:13:54 +0530

    remove mdx app

--- mdx-app/app/mdx-sample/test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function TEST() {
-  return (
-    <div>TEST</div>
-  )
-}
--- mdx-app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <div className="flex flex-col gap-2 justify-center items-center h-screen" >
-      <Link href="/mdx-sample" className="text-3xl">MDX sample</Link>
-    </div>
-  );
-}
